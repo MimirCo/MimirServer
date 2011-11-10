6516798f744d02b79c1cebced40addfb844b4f72
Rohan Jain <rohanj@stanford.edu>
2011-11-09 22:25:14 -0800

    Processing notification code

---
+++ mimir/process_notifications.py
@@ -0,0 +1,21 @@
+import setup_environment, urbanairship
+from server.models import User, Notification
+
+# Obtain all notifications that are not done.
+notif_list=Notification.objects.filter(done=False)
+
+# Setup urban airship
+airship = urbanairship.Airship('EkqcG1DpQcWBgekENCBSYw',
+			       '9U1SqeG1TuasAfVR4UNc9w')
+
+for notif in notif_list:
+  # Obtain user and device token
+  user = User.objects.get(user_id=notif.user_id)
+  device_token = user.device_token
+  # Push notification
+  airship.push({'aps': {'alert': notif.notification}},
+                device_tokens=[device_token],
+		schedule_for=str(notif.schedule_time))
+  # Mark the notification as done
+  notif.done = True
+  notif.save()
---
+++ mimir/setup_environment.py
@@ -0,0 +1,6 @@
+# Setting up environment
+from django.core.management import setup_environ
+import settings
+
+setup_environ(settings)
+
